2055656d3fbef5cb3f0eed4e18ea8d6b6a225fc0
Pen-Emkay <pen.em.kay@gmail.com>
2021-03-24 14:25:30 +0530

    started testing APIs

---
+++ webdev-bootcamp-api-proj03/main.js
@@ -0,0 +1,9 @@
+function onPageLoad() {
+    start();
+}
+
+async function start() {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    const data = await response.json();
+    console.log(data);
+}
