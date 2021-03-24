9341edf6dc10fa847663287974dbcbc5152fb403
Pen-Emkay <pen.em.kay@gmail.com>
2021-03-24 16:41:12 +0530

    Updated async function to comsume API service

--- webdev-bootcamp-api-proj03/main.js
+++ webdev-bootcamp-api-proj03/main.js
@@ -5,5 +5,16 @@ function onPageLoad() {
 async function start() {
     const response = await fetch("https://dog.ceo/api/breeds/list/all");
     const data = await response.json();
-    console.log(data);
+    createBreedList(data.message);
+}
+
+function createBreedList(breedList) {
+    document.getElementById("breed").innerHTML = `
+        <select>
+        <option>Choose a dog breed</option>
+        ${Object.keys(breedList).map(function(breed){
+               return `<option>${breed}</option>`
+        }).join('')}
+        </select>    
+    `;
 }
