303c2139772c6732fb785793559ac0f184b2dcf4
Pen-Emkay <pen.em.kay@gmail.com>
2021-03-25 08:07:04 +0530

    Slideshow working

--- webdev-bootcamp-api-proj03/main.js
+++ webdev-bootcamp-api-proj03/main.js
@@ -1,3 +1,6 @@
+let slideShowInterval;
+let firstPhotoDelay;
+
 function onPageLoad() {
     start();
 }
@@ -10,11 +13,43 @@ async function start() {
 
 function createBreedList(breedList) {
     document.getElementById("breed").innerHTML = `
-        <select>
+        <select onchange="loadByBreed(this.value)">
         <option>Choose a dog breed</option>
         ${Object.keys(breedList).map(function(breed){
                return `<option>${breed}</option>`
         }).join('')}
         </select>    
     `;
+}
+
+async function loadByBreed(selectedBreed){
+    if(selectedBreed!="Choose a dog breed")
+    {
+        const response= await fetch(`https://dog.ceo/api/breed/${selectedBreed}/images`);
+        const data=await response.json();
+        createSlideShow(data.message);
+    }
+    
+}
+
+function createSlideShow(imageList){
+    if (slideShowInterval) clearInterval(slideShowInterval)
+    if (firstPhotoDelay) clearTimeout(firstPhotoDelay);
+    let currentPosition=0;
+    document.getElementById("slideShow").innerHTML=`
+    <div class="slide" style="background-image: url('${imageList[0]}')"></div>
+    <div class="slide" style="background-image: url('${imageList[1]}')"></div>`;
+    currentPosition+=2;
+    slideShowInterval= setInterval(nextSlide, 3000 );
+    
+    function nextSlide()
+    {
+        document.getElementById("slideShow").insertAdjacentHTML("beforeend",`<div class="slide" style="background-image: url('${imageList[currentPosition]}')"></div>`);
+        firstPhotoDelay=setTimeout(function(){document.querySelector(".slide").remove();},1000);
+        if(currentPosition+1 >= imageList.length){
+            currentPosition=0;
+        }else{
+            currentPosition++;
+        }
+    }
 }
