a65477ee229249831bab1935742d5ca8b67d2c85
Pen-Emkay <pen.em.kay@gmail.com>
2021-04-06 16:02:15 +0530

    deleted undesired changes

---
+++ webdev-bootcamp-proj06-angular-test/src/index.js
@@ -0,0 +1,5 @@
+import validator from 'validator'
+import tripleMe from '.\tripleme'
+
+console.log("Manasi.kakodkar@gmail.com: " + validator.isEmail("Manasi.kakodkar@gmail.com"));
+console.log("Tripled value: " + tripleMe(3))
