f14f4db19194bd8e3f799f9631f4a7c9aa28d51b
Pen-Emkay <pen.em.kay@gmail.com>
2021-04-06 19:07:18 +0530

    webpack demo completed

--- webdev-bootcamp-proj06-angular-test/src/index.js
+++
@@ -1,5 +0,0 @@
-import validator from 'validator'
-import tripleMe from '.\tripleme'
-
-console.log("Manasi.kakodkar@gmail.com: " + validator.isEmail("Manasi.kakodkar@gmail.com"));
-console.log("Tripled value: " + tripleMe(3))
